208c08c120a690285430d0c1e4720034997586f2
hwangchahae <wndal12@gmail.com>
2025-09-05 07:14:05 +0900

    root&items

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI 
+from routers import items
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return{"hello":"world"}
+
+# 라우터 등록
+app.include_router(items.router, prefix="/items", tags=["items"])
---
+++ routers/items.py
@@ -0,0 +1,10 @@
+# routers/items.py
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+
+@router.get("/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
